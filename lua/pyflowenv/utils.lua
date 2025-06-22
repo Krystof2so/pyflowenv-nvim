54d10197be98e8f18f2b7530bdfcbfa99a09dead
Krystof2so <christo.2so@gmail.com>
2025-06-22 11:29:40 +0200

    [Doc] Comments in 'utils.lua'

--- lua/pyflowenv/utils.lua
+++ lua/pyflowenv/utils.lua
@@ -1,18 +1,27 @@
--- *************************************
--- ** pyflowenv/utils.lua             **
--- ** ------------------------------- **
--- ** - Fonctions génériques liées :  **
--- ** - Exécution de commandes shell. **
--- *************************************
+-- ***************************************
+-- ** pyflowenv/utils.lua               **
+-- ** --------------------------------- **
+-- ** FR : Fonctions génériques liées : **
+-- ** - Exécution de commandes shell.   **
+-- ** --------------------------------- **
+-- ** EN : Related generic functions:   **
+-- ** - Shell command execution.        **
+-- ***************************************
 
 
 local M = {}
 
+
+-- *********************************
+-- * Function check_run_in_shell() *
+-- *********************************
+-- FR : Prend en paramètre une commande shell ('cmd')
+-- EN : Takes a shell command (‘cmd’) as a parameter
 function M.check_run_in_shell(cmd)
-  vim.fn.system(cmd)
-  local success = vim.v.shell_error == 0
+  vim.fn.system(cmd)  -- executes the command.
+  local success = vim.v.shell_error == 0  -- checks whether the execution was successful
   return success
 end
 
-return M
 
+return M
